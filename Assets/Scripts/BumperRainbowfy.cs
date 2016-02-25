46b505c87c811c95e6068090048161753c2f138b
Tristan Burgess <bistan.turgess@gmail.com>
2016-02-25 14:13:52 -0500

    feature: Initial commit of Roller Madness game

---
+++ Assets/Scripts/BumperRainbowfy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumperRainbowfy : MonoBehaviour {
+    public Color[] colors;
+
+    public int currentIndex = 0;
+    private int nextIndex;
+
+    public float changeColourTime = 2.0f;
+
+    private float lastChange = 0.0f;
+    private float timer = 0.0f;
+
+    void Start()
+    {
+        nextIndex = (currentIndex + 1) % colors.Length;
+    }
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer > changeColourTime)
+        {
+            currentIndex = (currentIndex + 1) % colors.Length;
+            nextIndex = (currentIndex + 1) % colors.Length;
+            timer = 0.0f;
+
+        }
+        GetComponent<Renderer>().material.color = Color.Lerp(colors[currentIndex], colors[nextIndex], timer / changeColourTime);
+    }
+
+}
---
+++ Assets/Scripts/OnBumperCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class OnBumperCollision : MonoBehaviour
+{
+    public AudioClip impact;
+    new AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter()
+    {
+        audio.PlayOneShot(impact, 0.7F);
+    }
+}
